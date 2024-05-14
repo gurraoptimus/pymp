770c5f0313fde0298a89fcb1b01520505289032a
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-05-14 13:50:53 +0000

    Pymp

--- pymp.py
+++ pymp.py
@@ -1,3 +1,14 @@
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QIcon, QPalette
-from PyQt5.QtMul import 
+from PyQt5.QtMultimedia import QMediaPlayer
+from PyQt5.QtCore import Qt
+
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+
+
