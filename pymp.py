2e5f74cf1228ecb423860c69deaa638ee3219cf5
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-05-14 16:02:10 +0200

    Pymp

--- pymp.py
+++ pymp.py
@@ -2,13 +2,15 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QIcon, QPalette
 from PyQt5.QtMultimedia import QMediaPlayer
 from PyQt5.QtCore import Qt
-
+import sys
 
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.setWindowIcon(QIcon("player.ico"))
+        self.setWindowTitle("PYmediaPlayer")
+        self.setGeometry(350,100, 700,500)
 
-
-
+p = self.palette()
