ec71ca202892c914bc5c10a9c02474b8b5534ac9
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-06-08 15:15:16 +0200

    Pymp

--- pymp.py
+++ pymp.py
@@ -5,7 +5,6 @@ from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtCore import Qt, QUrl
 import sys
 
-
 class Window(QWidget):
     def __init__(self):
         super().__init__()
@@ -21,52 +20,36 @@ class Window(QWidget):
         self.create_player()
 
     def create_player(self):
-
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
-
         Videowidget = QVideoWidget()
-
 
         self.openBtn = QPushButton('Open Video')
         self.openBtn.clicked.connect(self.open_file)
-
 
         self.playBtn = QPushButton()
         self.playBtn.setEnabled(False)
         self.playBtn.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
 
-        
         self.slider = QSlider(Qt.Horizontal)
         self.slider.setRange(0,0)
-        
-        
+
         hbox = QHBoxLayout()
         hbox.setContentsMargins(0,0,0,0)
-
         hbox.addWidget(self.openBtn)
         hbox.addWidget(self.playBtn)
         hbox.addWidget(self.slider)
 
-
-
         vbox = QVBoxLayout()
         vbox.addWidget(Videowidget)
-
         vbox.addLayout(hbox)
-
 
         self.setLayout(vbox)
 
-
-
-
-def open_file(self):
-    filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
-
-
-    if filename != '':
-        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(filename)))
-        self.playBtn.setEnabled(True)
+    def open_file(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
+        if filename != '':
+            self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(filename)))
+            self.playBtn.setEnabled(True)
 
 app = QApplication(sys.argv)
 window = Window()
