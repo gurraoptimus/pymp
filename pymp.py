92b92f5db301b2b7fcb3a07275df9818aace10f1
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-06-10 21:49:55 +0200

    pymp

--- pymp.py
+++ pymp.py
@@ -61,8 +61,8 @@ class Window(QWidget):
     def mediastate_changed(self, state):
         if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
             self.playBtn.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
-        else:
-            
+        #else:
+            #
 
 app = QApplication(sys.argv)
 window = Window()
